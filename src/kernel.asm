78175d0ee5515c9149f0bd059cd1075a9c4936c4
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-10-12 11:54:41 +0200

    Finish 64 bit mode Cleanup Display some interface

--- src/kernel.asm
+++ src/kernel.asm
@@ -5,6 +5,39 @@ jmp _start
 
 %include "src/utils/intel_16.asm"
 
+%define BLACK_F 0x0
+%define BLUE_F 0x1
+%define GREEN_F 0x2
+%define CYAN_F 0x3
+%define RED_F 0x4
+%define PINK_F 0x5
+%define ORANGE_F 0x6
+%define WHITE_F 0x7
+
+%define BLACK_B 0x0
+%define BLUE_B 0x1
+%define GREEN_B 0x2
+%define CYAN_B 0x3
+%define RED_B 0x4
+%define PINK_B 0x5
+%define ORANGE_B 0x6
+%define WHITE_B 0x7
+
+%define STYLE(f,b) ((f << 4) + b)
+
+%macro PRINT_B 3
+    mov rdi, TRAM
+    mov rbx, %1
+    mov dl, STYLE(%2, %3)
+    call print_string
+%endmacro
+
+%macro PRINT_P 3
+    mov rbx, %1
+    mov dl, STYLE(%2, %3)
+    call print_string
+%endmacro
+
 _start:
     xor ax, ax
     mov ds, ax
@@ -44,8 +77,6 @@ _start:
 
 pm_start:
 
-    jmp $
-
     ; Update segments
     mov ax, DATA_SELECTOR-GDT64
     mov ds, ax
@@ -66,13 +97,13 @@ pm_start:
     rep stosd
 
     ; Update pages
-    mov     dword [0x70000], 0x71000 + 7    ; first PDP table
-    mov     dword [0x71000], 0x72000 + 7    ; first page directory
-    mov     dword [0x72000], 0x73000 + 7    ; first page table
+    mov dword [0x70000], 0x71000 + 7    ; first PDP table
+    mov dword [0x71000], 0x72000 + 7    ; first page directory
+    mov dword [0x72000], 0x73000 + 7    ; first page table
 
-    mov     edi, 0x73000                    ; address of first page table
-    mov     eax, 7
-    mov     ecx, 256                        ; number of pages to map (1 MB)
+    mov edi, 0x73000                    ; address of first page table
+    mov eax, 7
+    mov ecx, 256                        ; number of pages to map (1 MB)
 
     make_page_entries:
         stosd
@@ -80,12 +111,17 @@ pm_start:
         add     eax, 0x1000
         loop    make_page_entries
 
+    ; Update MSR (Model Specific Registers)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 100000000b
+    wrmsr
+
     ; Copy PML4 address into cr3
     mov     eax, 0x70000    ; Bass address of PML4
     mov     cr3, eax        ; load page-map level-4 base
 
     ; Switch to long mode
-
     mov eax, cr0
     or eax, 10000000000000000000000000000000b
     mov cr0, eax
@@ -95,14 +131,73 @@ pm_start:
 [BITS 64]
 
 lm_start:
+    ; Clean up all the screen
+    call clear_screen
 
+    ;Display the command line
+    mov rdi, TRAM + 0x14 * 8
+    PRINT_P command_line, BLACK_F, WHITE_B
+
+    ; Wait
     jmp $
+
+; Functions
+
+clear_screen:
+    ; Print top bar
+    PRINT_B header_title, WHITE_F, BLACK_B
+
+    mov rdi, TRAM + 0x14 * 8
+
+    mov rcx, 0x14 * 24
+    mov rax, 0x0720072007200720
+    rep stosq
+
+    ret
+
+; rbx = address of string to print
+; rdi = START of write
+; dl = code style
+print_string:
+    push rax
+
+.repeat:
+    ; Read the char
+    mov al, [rbx]
+
+    ; Test if end of string
+    cmp al, 0
+    je .done
+
+    ; Write char
+    stosb
+
+    ; Write style code
+    mov al, dl
+    stosb
+
+    inc rbx
+
+    jmp .repeat
+
+.done:
+    pop rax
+
+    ret
 
 ; Datas
 
     kernel_header_0 db '******************************', 0
     kernel_header_1 db 'Welcome to Thor OS!', 0
     kernel_header_2 db '******************************', 0
+
+    header_title db "                                    THOR OS                                     ", 0
+    command_line db "thor> ", 0
+
+; Constants
+
+    TRAM equ 0x0B8000 ; Text RAM
+    VRAM equ 0x0A0000 ; Video RAM
 
 ; Global Descriptors Table
 
@@ -136,4 +231,4 @@ GDT64:
    GDT_LENGTH:
 
    ; Fill the sector (not necessary, but cleaner)
-   times 512-($-$$) db 0
+   times 1024-($-$$) db 0
