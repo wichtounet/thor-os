ed91c5ce5ab18ddf909386ce5845074049a2ddd7
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-11-04 19:14:42 +0100

    Fix interruptions

--- micro_kernel/interrupts.asm
+++ micro_kernel/interrupts.asm
@@ -111,10 +111,10 @@ _isr%1:
 
 %macro CREATE_IRQ 1
 _irq%1:
-    push rax
-
     ; Disable interruptions to avoid being interrupted
     cli
+
+    push rax
 
     mov rax, [irq_handlers + 8 *%1]
 
@@ -122,8 +122,28 @@ _irq%1:
     test rax, rax
     je .eoi
 
+    push rax
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+
     ; Call the handler
     call rax
+
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rax
 
     .eoi:
 
