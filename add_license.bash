24f4211884d3ef193a309f8e7879b58999af0884
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-11-06 22:00:22 +0100

    Improve license generation

--- add_license.bash
+++ add_license.bash
@@ -1,6 +1,11 @@
 for file in "$@"
 do
-    cp ${file} ${file}.orig
-    cat license_header ${file}.orig > ${file}
-    rm ${file}.orig
+    lines=`grep "Distributed under the Boost Software License" $file | wc -l`
+
+    if [[ $lines == 0 ]]
+    then
+        cp ${file} ${file}.orig
+        cat license_header ${file}.orig > ${file}
+        rm ${file}.orig
+    fi
 done
