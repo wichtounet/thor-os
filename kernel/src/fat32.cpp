843c95dd0c9928c2d618780b2acd37e0e193c369
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-11-09 21:13:50 +0100

    Cleanup

--- kernel/src/fat32.cpp
+++ kernel/src/fat32.cpp
@@ -115,8 +115,7 @@ void fat32::ls(const disks::disk_descriptor& disk, const disks::partition_descri
                         //It is a normal file name
                     }
 
-                    k_print(entry.name, 11);
-                    k_print_line();
+                    k_print_line(entry.name, 11);
 
                     if(entry.attrib & 0x10){
                         k_print_line("Directory");
@@ -124,8 +123,7 @@ void fat32::ls(const disks::disk_descriptor& disk, const disks::partition_descri
                         k_print_line("File");
                     }
 
-                    k_print(static_cast<uint64_t>(entry.file_size));
-                    k_print_line();
+                    k_print_line(static_cast<uint64_t>(entry.file_size));
                 }
             }
         }
