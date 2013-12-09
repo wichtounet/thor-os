ab5369b15ad440852efdf133da0eb8eaf2243921
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-12-09 18:14:10 +0100

    Activate SSE support

--- micro_kernel/micro_kernel.asm
+++ micro_kernel/micro_kernel.asm
@@ -172,11 +172,36 @@ lm_start:
     ; Install custom syscalls
     call install_syscalls
 
+    call enable_sse
+
     sti
 
     call 0x5000
 
     jmp $
+
+; Functions
+
+enable_sse:
+    ; Test if SSE is supported by the processor
+    mov eax, 0x1
+    cpuid
+    test edx, 1<<25
+    jz .no_sse
+
+    ; Enable SSE support
+    xor rax, rax
+    mov rax, cr0
+    and ax, 0xFFFB  ;clear coprocessor emulation CR0.EM
+    or ax, 0x2      ;set coprocessor monitoring  CR0.MP
+    mov cr0, rax
+    mov rax, cr4
+    or ax, 3 << 9   ;set CR4.OSFXSR and CR4.OSXMMEXCPT
+    mov cr4, rax
+
+    .no_sse:
+
+    ret
 
 ; Includes
 
