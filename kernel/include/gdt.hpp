0c30f2ef46d66deb66b4ad0d71b7a6adcb518fb3
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-12-15 21:54:53 +0100

    Cleanup

---
+++ kernel/include/gdt.hpp
@@ -0,0 +1,19 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef GDT_H
+#define GDT_H
+
+namespace gdt {
+
+constexpr const uint16_t CODE_SELECTOR = 0x08;
+constexpr const uint16_t DATA_SELECTOR = 0x10;;
+constexpr const uint16_t LONG_SELECTOR = 0x18;;
+
+} //end of namespace gdt
+
+#endif
--- kernel/src/interrupts.cpp
+++ kernel/src/interrupts.cpp
@@ -10,6 +10,7 @@
 #include "utils.hpp"
 #include "console.hpp"
 #include "kernel_utils.hpp"
+#include "gdt.hpp"
 
 #include "isrs.hpp"
 #include "irqs.hpp"
@@ -116,38 +117,38 @@ void interrupt::install_idt(){
 void interrupt::install_isrs(){
     //TODO The GDT Selector should be computed in a better way
 
-    idt_set_gate(0, _isr0, 0x18, 0x8E);
-    idt_set_gate(1, _isr1, 0x18, 0x8E);
-    idt_set_gate(2, _isr2, 0x18, 0x8E);
-    idt_set_gate(3, _isr3, 0x18, 0x8E);
-    idt_set_gate(4, _isr4, 0x18, 0x8E);
-    idt_set_gate(5, _isr5, 0x18, 0x8E);
-    idt_set_gate(6, _isr6, 0x18, 0x8E);
-    idt_set_gate(7, _isr7, 0x18, 0x8E);
-    idt_set_gate(8, _isr8, 0x18, 0x8E);
-    idt_set_gate(9, _isr9, 0x18, 0x8E);
-    idt_set_gate(10, _isr10, 0x18, 0x8E);
-    idt_set_gate(11, _isr11, 0x18, 0x8E);
-    idt_set_gate(12, _isr12, 0x18, 0x8E);
-    idt_set_gate(13, _isr13, 0x18, 0x8E);
-    idt_set_gate(14, _isr14, 0x18, 0x8E);
-    idt_set_gate(15, _isr15, 0x18, 0x8E);
-    idt_set_gate(16, _isr16, 0x18, 0x8E);
-    idt_set_gate(17, _isr17, 0x18, 0x8E);
-    idt_set_gate(18, _isr18, 0x18, 0x8E);
-    idt_set_gate(19, _isr19, 0x18, 0x8E);
-    idt_set_gate(20, _isr20, 0x18, 0x8E);
-    idt_set_gate(21, _isr21, 0x18, 0x8E);
-    idt_set_gate(22, _isr22, 0x18, 0x8E);
-    idt_set_gate(23, _isr23, 0x18, 0x8E);
-    idt_set_gate(24, _isr24, 0x18, 0x8E);
-    idt_set_gate(25, _isr25, 0x18, 0x8E);
-    idt_set_gate(26, _isr26, 0x18, 0x8E);
-    idt_set_gate(27, _isr27, 0x18, 0x8E);
-    idt_set_gate(28, _isr28, 0x18, 0x8E);
-    idt_set_gate(29, _isr29, 0x18, 0x8E);
-    idt_set_gate(30, _isr30, 0x18, 0x8E);
-    idt_set_gate(31, _isr31, 0x18, 0x8E);
+    idt_set_gate(0, _isr0, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(1, _isr1, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(2, _isr2, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(3, _isr3, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(4, _isr4, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(5, _isr5, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(6, _isr6, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(7, _isr7, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(8, _isr8, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(9, _isr9, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(10, _isr10, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(11, _isr11, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(12, _isr12, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(13, _isr13, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(14, _isr14, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(15, _isr15, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(16, _isr16, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(17, _isr17, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(18, _isr18, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(19, _isr19, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(20, _isr20, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(21, _isr21, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(22, _isr22, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(23, _isr23, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(24, _isr24, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(25, _isr25, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(26, _isr26, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(27, _isr27, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(28, _isr28, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(29, _isr29, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(30, _isr30, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(31, _isr31, gdt::LONG_SELECTOR, 0x8E);
 }
 
 void interrupt::remap_irqs(){
@@ -174,22 +175,22 @@ void interrupt::remap_irqs(){
 void interrupt::install_irqs(){
     //TODO The GDT Selector should be computed in a better way
 
-    idt_set_gate(32, _irq0, 0x18, 0x8E);
-    idt_set_gate(33, _irq1, 0x18, 0x8E);
-    idt_set_gate(34, _irq2, 0x18, 0x8E);
-    idt_set_gate(35, _irq3, 0x18, 0x8E);
-    idt_set_gate(36, _irq4, 0x18, 0x8E);
-    idt_set_gate(37, _irq5, 0x18, 0x8E);
-    idt_set_gate(38, _irq6, 0x18, 0x8E);
-    idt_set_gate(39, _irq7, 0x18, 0x8E);
-    idt_set_gate(40, _irq8, 0x18, 0x8E);
-    idt_set_gate(41, _irq9, 0x18, 0x8E);
-    idt_set_gate(42, _irq10, 0x18, 0x8E);
-    idt_set_gate(43, _irq11, 0x18, 0x8E);
-    idt_set_gate(44, _irq12, 0x18, 0x8E);
-    idt_set_gate(45, _irq13, 0x18, 0x8E);
-    idt_set_gate(46, _irq14, 0x18, 0x8E);
-    idt_set_gate(47, _irq15, 0x18, 0x8E);
+    idt_set_gate(32, _irq0, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(33, _irq1, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(34, _irq2, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(35, _irq3, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(36, _irq4, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(37, _irq5, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(38, _irq6, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(39, _irq7, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(40, _irq8, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(41, _irq9, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(42, _irq10, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(43, _irq11, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(44, _irq12, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(45, _irq13, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(46, _irq14, gdt::LONG_SELECTOR, 0x8E);
+    idt_set_gate(47, _irq15, gdt::LONG_SELECTOR, 0x8E);
 }
 
 void interrupt::register_irq_handler(size_t irq, void (*handler)()){
