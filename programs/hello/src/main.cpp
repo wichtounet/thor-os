e46b1fc015b5a75d2242a521546e1a1c37788b5c
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-01-09 22:43:26 +0100

    Clean

--- programs/hello/src/main.cpp
+++ programs/hello/src/main.cpp
@@ -16,7 +16,7 @@ uint8_t make_color(uint8_t fg, uint8_t bg){
     return fg | bg << 4;
 }
 
-uint16_t make_vga_entry(char c, uint8_t color){
+uint16_t make_vga_entry(const char c, uint8_t color){
     uint16_t c16 = c;
     uint16_t color16 = color;
     return c16 | color16 << 8;
