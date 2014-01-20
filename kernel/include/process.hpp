79c2c956a1a8cb82b5056577144be1aa38251d8a
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-01-20 22:18:30 +0100

    Prepare structures for scheduling

---
+++ kernel/include/process.hpp
@@ -0,0 +1,19 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef PROCESS_H
+#define PROCESS_H
+
+namespace scheduler {
+
+struct process_t {
+
+};
+
+} //end of namespace scheduler
+
+#endif
---
+++ kernel/include/scheduler.hpp
@@ -0,0 +1,18 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+namespace scheduler {
+
+void init();
+void start();
+
+} //end of namespace scheduler
+
+#endif
