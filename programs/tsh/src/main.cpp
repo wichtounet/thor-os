7fbd019e8a45f112c6afe2b5de056832a82e0b32
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-02-03 20:24:24 +0100

    Improve shell

--- programs/tsh/src/main.cpp
+++ programs/tsh/src/main.cpp
@@ -16,16 +16,27 @@ int main(){
         print("thor> ");
 
         auto c = read_input(input_buffer, 63);
-        input_buffer[c-1] = '\0';
 
-        if(str_equals(input_buffer, "exit")){
-            exit(0);
-        } else if(str_equals(input_buffer, "sleep")){
-            //TODO Once better infrastrucure, parse command line and sleep the
-            //correct number of milliseconds
-            sleep_ms(5000);
+        if(input_buffer[c-1] == '\n'){
+            if(c == 1){
+                print_line();
+                continue;
+            }
+
+            input_buffer[c] = '\0';
+
+            if(str_equals(input_buffer, "exit")){
+                exit(0);
+            } else if(str_equals(input_buffer, "sleep")){
+                //TODO Once better infrastrucure, parse command line and sleep the
+                //correct number of milliseconds
+                sleep_ms(5000);
+            } else {
+                print_line("Unknown command");
+            }
         } else {
-            print_line("Unknown command");
+            //TODO Once in good shape, just append to current input buffer
         }
+
     }
 }
