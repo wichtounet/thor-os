d72bf1d987370a87bfe6748e5964de11fa37b7ca
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-02-07 19:41:15 +0100

    Extract bitmaps from virtual_allocator

--- kernel/src/virtual_allocator.cpp
+++ kernel/src/virtual_allocator.cpp
@@ -11,6 +11,7 @@
 #include "paging.hpp"
 #include "console.hpp"
 #include "kernel.hpp"
+#include "bitmap.hpp"
 
 namespace {
 
@@ -21,83 +22,6 @@ constexpr const size_t managed_space = last_virtual_address - first_virtual_addr
 constexpr const size_t unit = paging::PAGE_SIZE;
 
 size_t allocated_pages = first_virtual_address / paging::PAGE_SIZE;
-
-struct static_bitmap {
-    typedef uint64_t data_type;
-
-    static constexpr const size_t bits_per_word = sizeof(data_type) * 8;
-
-    size_t words;
-    data_type* data;
-
-    template<typename Array>
-    void init(Array& array){
-        words = array.size();
-        data = array.data();
-    }
-
-    static constexpr size_t word_offset(size_t bit){
-        return bit / bits_per_word;
-    }
-
-    static constexpr size_t bit_offset(size_t bit){
-        return bit % bits_per_word;
-    }
-
-    static constexpr data_type bit_mask(size_t bit){
-        return static_cast<data_type>(1) << bit_offset(bit);
-    }
-
-    void clear_all(){
-        for(size_t i = 0; i < words; ++i){
-            data[i] = 0;
-        }
-    }
-
-    void set_all(){
-        for(size_t i = 0; i < words; ++i){
-            data[i] = ~static_cast<data_type>(0);
-        }
-    }
-
-    size_t free_bit() const {
-        for(size_t w = 0; w < words; ++w){
-            if(data[w] > 0){
-                for(size_t b = 0; b < bits_per_word; ++b){
-                    if(data[w] & bit_mask(b)){
-                        return w * bits_per_word + b;
-                    }
-                }
-            }
-        }
-
-        //TODO Use an assert here
-        return 0;
-    }
-
-    size_t free_word() const {
-        for(size_t w = 0; w < words; ++w){
-            if(data[w] == ~static_cast<data_type>(0)){
-                return w * bits_per_word;
-            }
-        }
-
-        //TODO Use an assert here
-        return 0;
-    }
-
-    bool is_set(size_t bit) const {
-        return data[word_offset(bit)] & bit_mask(bit);
-    }
-
-    void set(size_t bit){
-        data[word_offset(bit)] |= bit_mask(bit);
-    }
-
-    void unset(size_t bit){
-        data[word_offset(bit)] &= ~bit_mask(bit);
-    }
-};
 
 constexpr size_t array_size(int block){
     return (managed_space / (block * unit) + 1) / (sizeof(uint64_t) * 8) + 1;
