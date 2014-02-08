6b505e6a72fbf465c0cff3a66d30d2e4e5a7735f
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-02-08 16:55:16 +0100

    Adapt the userlib to compile it as static library

--- cpp.mk
+++ cpp.mk
@@ -1,6 +1,7 @@
 CC=x86_64-elf-g++
 AS=x86_64-elf-as
 OC=x86_64-elf-objcopy
+AR=x86_64-elf-ar
 
 WARNING_FLAGS=-Wall -Wextra -pedantic -Wold-style-cast -Wshadow
 COMMON_CPP_FLAGS=-masm=intel -Iinclude/ -nostdlib -g -Os -std=c++11 -fno-stack-protector -fno-exceptions -funsigned-char -fno-rtti -ffreestanding -fomit-frame-pointer -mno-red-zone -mno-3dnow -mno-mmx -fno-asynchronous-unwind-tables
@@ -12,6 +13,9 @@ CPP_FLAGS_32=$(COMMON_CPP_FLAGS) $(CPP_FLAGS_LOW) -mpreferred-stack-boundary=4
 CPP_FLAGS_64=$(COMMON_CPP_FLAGS) -mno-sse -mno-sse2 -mno-sse3 -mno-sse4 -mno-sse4.1 -mno-sse4.2 -mno-avx
 
 COMMON_LINK_FLAGS=-lgcc
-PROGRAM_LINK_FLAGS=$(COMMON_LINK_FLAGS) -mcmodel=small -fPIC -z max-page-size=0x1000 -T ../linker.ld
 
-PROGRAM_FLAGS=-I../../userlib/include/ -mcmodel=small -fPIC
+LIB_FLAGS=$(CPP_FLAGS_64) $(WARNING_FLAGS) -mcmodel=small -fPIC
+LIB_LINK_FLAGS=$(CPP_FLAGS_64) $(WARNING_FLAGS) -mcmodel=small -fPIC
+
+PROGRAM_FLAGS=$(CPP_FLAGS_64) $(WARNING_FLAGS) -I../../userlib/include/ -static -L../../userlib/ -ltlib -mcmodel=small -fPIC
+PROGRAM_LINK_FLAGS=$(CPP_FLAGS_64) $(WARNING_FLAGS) $(COMMON_LINK_FLAGS) -static -L../../userlib/ -ltlib -mcmodel=small -fPIC -z max-page-size=0x1000 -T ../linker.ld
--- userlib/include/string.hpp
+++ userlib/include/string.hpp
@@ -8,33 +8,7 @@
 #ifndef USERLIB_STRING_H
 #define USERLIB_STRING_H
 
-int str_compare(const char *s1, const char *s2){
-    while (*s1 != '\0') {
-        if (*s2 == '\0'){
-            return  1;
-        }
-
-        if (*s2 > *s1){
-            return -1;
-        }
-
-        if (*s1 > *s2){
-            return  1;
-        }
-
-        s1++;
-        s2++;
-    }
-
-    if (*s2 != '\0'){
-        return -1;
-    }
-
-    return 0;
-}
-
-bool str_equals(const char* s1, const char* s2){
-    return str_compare(s1, s2) == 0;
-}
+int str_compare(const char *s1, const char *s2);
+bool str_equals(const char* s1, const char* s2);
 
 #endif
