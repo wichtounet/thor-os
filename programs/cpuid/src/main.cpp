540f33935ce7fd875a3eedf289068a08066f93bf
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-02-09 22:53:23 +0100

    Continue cpuid implementation

--- programs/cpuid/src/main.cpp
+++ programs/cpuid/src/main.cpp
@@ -22,9 +22,23 @@ void native_cpuid(uint32_t key, uint32_t* eax, uint32_t* ebx, uint32_t* ecx, uin
         : "0" (*eax), "2" (*ecx));
 }
 
+void get_base_info(){
+    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
+
+    native_cpuid(1, &eax, &ebx, &ecx, &edx);
+
+    printf("Stepping: %u\n", eax & 0xF);
+    printf("Model: %u\n", (eax >> 4) & 0xF);
+    printf("Family: %u\n", (eax >> 8) & 0xF);
+    printf("Processor Type: %u\n", (eax >> 12) & 0x3);
+    printf("Extended Model: %u\n", (eax >> 16) & 0xF);
+    printf("Extended Family: %u\n", (eax >> 20) & 0xFF);
+}
+
 } //end of anonymous namespace
 
 int main(){
+    get_base_info();
 
     exit(0);
 }
