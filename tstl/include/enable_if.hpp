451bfe7a4b52a52a9abc5febf2e2091a5681b3f7
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-02-09 16:58:37 +0100

    Add helper types

--- tstl/include/enable_if.hpp
+++ tstl/include/enable_if.hpp
@@ -16,11 +16,17 @@ struct enable_if {};
 template<class T>
 struct enable_if<true, T> { typedef T type; };
 
+template< bool B, class T = void >
+using enable_if_t = typename enable_if<B, T>::type;
+
 template<bool B, class T = void>
 struct disable_if {};
 
 template<class T>
 struct disable_if<false, T> { typedef T type; };
+
+template< bool B, class T = void >
+using disable_if_t = typename disable_if<B, T>::type;
 
 } //end of namespace std
 
