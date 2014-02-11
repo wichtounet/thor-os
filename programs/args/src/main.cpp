21c892b2874fb350c0c5944fffa85619f8e29b1a
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-02-11 20:34:58 +0100

    Add test program to test args

---
+++ programs/args/src/main.cpp
@@ -0,0 +1,14 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include <print.hpp>
+#include <system.hpp>
+
+int main(int argc, char* argv[]){
+    print_line(argc);
+    exit(0);
+}
