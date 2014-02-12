a48d497a085da832aaf1ab4aa1eec5defda5435b
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-02-12 16:06:06 +0100

    Move errors.hpp into tlib

--- cpp.mk
+++ cpp.mk
@@ -4,7 +4,7 @@ OC=x86_64-elf-objcopy
 AR=x86_64-elf-ar
 
 WARNING_FLAGS=-Wall -Wextra -pedantic -Wold-style-cast -Wshadow
-COMMON_CPP_FLAGS=-masm=intel -I../../tstl/include/ -I../tstl/include/ -Iinclude/ -nostdlib -g -Os -std=c++11 -fno-stack-protector -fno-exceptions -funsigned-char -fno-rtti -ffreestanding -fomit-frame-pointer -mno-red-zone -mno-3dnow -mno-mmx -fno-asynchronous-unwind-tables
+COMMON_CPP_FLAGS=-masm=intel -I../../tstl/include/ -I../tstl/include/ -I../tlib/include/ -Iinclude/ -nostdlib -g -Os -std=c++11 -fno-stack-protector -fno-exceptions -funsigned-char -fno-rtti -ffreestanding -fomit-frame-pointer -mno-red-zone -mno-3dnow -mno-mmx -fno-asynchronous-unwind-tables
 
 CPP_FLAGS_LOW=-march=i386 -m32 -fno-strict-aliasing -fno-pic -fno-toplevel-reorder -mno-sse -mno-sse2 -mno-sse3 -mno-sse4 -mno-sse4.1 -mno-sse4.2
 
