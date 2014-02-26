b818980b474b11987753b93508f5eb63e158d15e
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-02-26 20:51:59 +0100

    Create program date

---
+++ programs/date/src/main.cpp
@@ -0,0 +1,27 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include <system.hpp>
+#include <print.hpp>
+
+int main(int, char*[]){
+    auto date = local_date();
+
+    print(date.day);
+    print('.');
+    print(date.month);
+    print('.');
+    print(date.year);
+    print(' ');
+
+    print(date.hour);
+    print(':');
+    print(date.minutes);
+    print_line();
+
+    exit(0);
+}
