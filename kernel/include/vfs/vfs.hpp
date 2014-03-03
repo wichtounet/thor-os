5075f7df587b294a4e7205d8d70a080d2cc4d3e6
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-03-03 20:23:05 +0100

    Cleanup

--- kernel/include/vfs/vfs.hpp
+++ kernel/include/vfs/vfs.hpp
@@ -22,18 +22,20 @@ enum class partition_type {
 void init();
 
 int64_t open(const char* file, size_t flags);
-int64_t statfs(const char* mount_point, statfs_info& info);
+void close(size_t fd);
+
 int64_t stat(size_t fd, stat_info& info);
+int64_t statfs(const char* mount_point, statfs_info& info);
 int64_t mkdir(const char* file);
 int64_t rm(const char* file);
 int64_t read(size_t fd, char* buffer, size_t count, size_t offset = 0);
-int64_t direct_read(const std::string& file, std::string& content);
 int64_t entries(size_t fd, char* buffer, size_t size);
 int64_t mounts(char* buffer, size_t size);
 
 int64_t mount(partition_type type, const char* mount_point, const char* device);
 
-void close(size_t fd);
+//Used only inside kernel as a easy way to read a complete file
+int64_t direct_read(const std::string& file, std::string& content);
 
 } //end of namespace vfs
 
