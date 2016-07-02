c3f68682fa3c6bc025b133b775f1c302f166314b
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-07-02 16:31:22 +0200

    Fix include

--- kernel/include/arch.hpp
+++ kernel/include/arch.hpp
@@ -5,6 +5,8 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 //=======================================================================
 
+#include <types.hpp>
+
 namespace arch {
 
 void enable_sse();
