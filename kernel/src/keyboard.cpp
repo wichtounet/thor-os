34161e5ac327087dc266c464281f947e1bf0d9fc
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-07-02 16:52:43 +0200

    Retab

--- kernel/src/keyboard.cpp
+++ kernel/src/keyboard.cpp
@@ -13,81 +13,81 @@
 namespace {
 
 char qwertz[128] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-  '9', '0', '-', '=', '\b',	/* Backspace */
-  '\t',			/* Tab */
-  'q', 'w', 'e', 'r',	/* 19 */
-  't', 'z', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
- '\'', '`',   0,		/* Left shift */
- '\\', 'y', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-  'm', ',', '.', '/',   0,				/* Right shift */
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8', /* 9 */
+  '9', '0', '-', '=', '\b', /* Backspace */
+  '\t',         /* Tab */
+  'q', 'w', 'e', 'r',   /* 19 */
+  't', 'z', 'u', 'i', 'o', 'p', '[', ']', '\n', /* Enter key */
+    0,          /* 29   - Control */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', /* 39 */
+ '\'', '`',   0,        /* Left shift */
+ '\\', 'y', 'x', 'c', 'v', 'b', 'n',            /* 49 */
+  'm', ',', '.', '/',   0,              /* Right shift */
   '*',
-    0,	/* Alt */
-  ' ',	/* Space bar */
-    0,	/* Caps lock */
-    0,	/* 59 - F1 key ... > */
+    0,  /* Alt */
+  ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
     0,   0,   0,   0,   0,   0,   0,   0,
-    0,	/* < ... F10 */
-    0,	/* 69 - Num lock*/
-    0,	/* Scroll Lock */
-    0,	/* Home key */
-    0,	/* Up Arrow */
-    0,	/* Page Up */
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
   '-',
-    0,	/* Left Arrow */
+    0,  /* Left Arrow */
     0,
-    0,	/* Right Arrow */
+    0,  /* Right Arrow */
   '+',
-    0,	/* 79 - End key*/
-    0,	/* Down Arrow */
-    0,	/* Page Down */
-    0,	/* Insert Key */
-    0,	/* Delete Key */
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
     0,   0,   0,
-    0,	/* F11 Key */
-    0,	/* F12 Key */
-    0,	/* All other keys are undefined */
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
 };
 
 char shifted_qwertz[128] = {
-    0,  27, '1', '2', '3', '4', '5', '6', '/', '8',	/* 9 */
-  '9', '0', '_', '=', '\b',	/* Backspace */
-  '\t',			/* Tab */
-  'Q', 'W', 'E', 'R',	/* 19 */
-  'T', 'Z', 'U', 'I', 'O', 'P', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';',	/* 39 */
- '\'', '`',   0,		/* Left shift */
- '\\', 'Y', 'X', 'C', 'V', 'B', 'N',			/* 49 */
-  'M', ',', '.', '/',   0,				/* Right shift */
+    0,  27, '1', '2', '3', '4', '5', '6', '/', '8', /* 9 */
+  '9', '0', '_', '=', '\b', /* Backspace */
+  '\t',         /* Tab */
+  'Q', 'W', 'E', 'R',   /* 19 */
+  'T', 'Z', 'U', 'I', 'O', 'P', '[', ']', '\n', /* Enter key */
+    0,          /* 29   - Control */
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', /* 39 */
+ '\'', '`',   0,        /* Left shift */
+ '\\', 'Y', 'X', 'C', 'V', 'B', 'N',            /* 49 */
+  'M', ',', '.', '/',   0,              /* Right shift */
   '*',
-    0,	/* Alt */
-  ' ',	/* Space bar */
-    0,	/* Caps lock */
-    0,	/* 59 - F1 key ... > */
+    0,  /* Alt */
+  ' ',  /* Space bar */
+    0,  /* Caps lock */
+    0,  /* 59 - F1 key ... > */
     0,   0,   0,   0,   0,   0,   0,   0,
-    0,	/* < ... F10 */
-    0,	/* 69 - Num lock*/
-    0,	/* Scroll Lock */
-    0,	/* Home key */
-    0,	/* Up Arrow */
-    0,	/* Page Up */
+    0,  /* < ... F10 */
+    0,  /* 69 - Num lock*/
+    0,  /* Scroll Lock */
+    0,  /* Home key */
+    0,  /* Up Arrow */
+    0,  /* Page Up */
   '-',
-    0,	/* Left Arrow */
+    0,  /* Left Arrow */
     0,
-    0,	/* Right Arrow */
+    0,  /* Right Arrow */
   '+',
-    0,	/* 79 - End key*/
-    0,	/* Down Arrow */
-    0,	/* Page Down */
-    0,	/* Insert Key */
-    0,	/* Delete Key */
+    0,  /* 79 - End key*/
+    0,  /* Down Arrow */
+    0,  /* Page Down */
+    0,  /* Insert Key */
+    0,  /* Delete Key */
     0,   0,   0,
-    0,	/* F11 Key */
-    0,	/* F12 Key */
-    0,	/* All other keys are undefined */
+    0,  /* F11 Key */
+    0,  /* F12 Key */
+    0,  /* All other keys are undefined */
 };
 
 void keyboard_handler(interrupt::syscall_regs*){
