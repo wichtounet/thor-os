40049a75cf25a28de915744cd40a2820bbaa37c2
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-07-02 16:42:45 +0200

    Update license copyright

--- bootloader/intel_16.asm
+++ bootloader/intel_16.asm
@@ -1,5 +1,5 @@
 ;=======================================================================
-; Copyright Baptiste Wicht 2013-2014.
+; Copyright Baptiste Wicht 2013-2016.
 ; Distributed under the Boost Software License, Version 1.0.
 ; (See accompanying file LICENSE_1_0.txt or copy at
 ;  http://www.boost.org/LICENSE_1_0.txt)
