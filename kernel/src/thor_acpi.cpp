006586177aa360f6d5f3f5360cb31eb4cd4039b3
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-07-23 13:26:03 +0200

    More ACPI OSL

--- kernel/src/thor_acpi.cpp
+++ kernel/src/thor_acpi.cpp
@@ -17,7 +17,42 @@
 #include "virtual_allocator.hpp"
 #include "paging.hpp"
 
+#include "mutex.hpp"
+#include "semaphore.hpp"
+
 extern "C" {
+
+// Initialization
+
+ACPI_STATUS AcpiOsInitialize(){
+    //Nothing to initialize
+
+    return AE_OK;
+}
+
+ACPI_STATUS AcpiOsTerminate(){
+    //Nothing to terminate
+
+    return AE_OK;
+}
+
+// Overriding of ACPI
+
+ACPI_STATUS AcpiOsPredefinedOverride(const ACPI_PREDEFINED_NAMES* /*PredefinedObject*/, ACPI_STRING* new_value){
+    *new_value = nullptr;
+    return AE_OK;
+}
+
+ACPI_STATUS AcpiOsTableOverride(ACPI_TABLE_HEADER* /*ExistingTable*/, ACPI_TABLE_HEADER** new_table){
+    *new_table = nullptr;
+    return AE_OK;
+}
+
+ACPI_STATUS AcpiOsPhysicalTableOverride(ACPI_TABLE_HEADER* /*ExistingTable*/, ACPI_PHYSICAL_ADDRESS* new_address, UINT32* new_table_length){
+    *new_address = 0;
+    *new_table_length = 0;
+    return AE_OK;
+}
 
 // Dynamic allocation
 
@@ -80,6 +115,76 @@ void AcpiOsUnmapMemory(void* virt_aligned_raw, ACPI_SIZE length){
 
     paging::unmap_pages(virt, pages);
     virtual_allocator::free(virt, pages);
+}
+
+// Concurrency
+
+#if (ACPI_MUTEX_TYPE != ACPI_BINARY_SEMAPHORE)
+
+ACPI_STATUS AcpiOsCreateMutex(ACPI_MUTEX* handle){
+    auto* lock = new mutex<false>();
+
+    lock->init();
+
+    *handle = lock;
+
+    return AE_OK;
+}
+
+void AcpiOsDeleteMutex(ACPI_MUTEX handle){
+    auto* lock = static_cast<mutex<false>*>(handle);
+
+    delete lock;
+}
+
+ACPI_STATUS AcpiOsAcquireMutex(ACPI_MUTEX handle, UINT16 Timeout){
+    auto* lock = static_cast<mutex<false>*>(handle);
+
+    lock->acquire();
+
+    return AE_OK;
+}
+
+void AcpiOsReleaseMutex(ACPI_MUTEX handle){
+    auto* lock = static_cast<mutex<false>*>(handle);
+
+    lock->release();
+}
+
+#endif
+
+ACPI_STATUS AcpiOsCreateSemaphore(UINT32 /*maxUnits*/, UINT32 initialUnits, ACPI_SEMAPHORE* handle){
+    auto* lock = new semaphore();
+
+    lock->init(initialUnits);
+
+    *handle = lock;
+
+    return AE_OK;
+}
+
+ACPI_STATUS AcpiOsDeleteSemaphore(ACPI_SEMAPHORE handle){
+    auto* lock = static_cast<semaphore*>(handle);
+
+    delete lock;
+
+    return AE_OK;
+}
+
+ACPI_STATUS AcpiOsWaitSemaphore(ACPI_SEMAPHORE handle, UINT32 units, UINT16 timeout){
+    auto* lock = static_cast<semaphore*>(handle);
+
+    lock->acquire();
+
+    return AE_OK;
+}
+
+ACPI_STATUS AcpiOsSignalSemaphore(ACPI_SEMAPHORE handle, UINT32 units){
+    auto* lock = static_cast<semaphore*>(handle);
+
+    lock->release();
+
+    return AE_OK;
 }
 
 } //end of extern "C"
