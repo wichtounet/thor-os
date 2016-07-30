5a665089cb16f4e31bc4b1ba38dd108aac51d4ba
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-07-30 22:06:35 +0200

    Prepare HPET support

---
+++ kernel/src/drivers/hpet.cpp
@@ -0,0 +1,16 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2016.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include "drivers/hpet.hpp"
+
+namespace {
+
+} //End of anonymous namespace
+
+bool hpet::install(){
+    return false;
+}
