bc3ef4e28c77f62e620228d2bf7cef42d5d79789
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-15 21:25:00 +0200

    Prepare a path abstraction

---
+++ kernel/include/vfs/path.hpp
@@ -0,0 +1,32 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2016.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef VFS_PATH_H
+#define VFS_PATH_H
+
+#include <vector.hpp>
+#include <string.hpp>
+
+/*!
+ * \brief Structure to represent a path on the file system
+ */
+struct path {
+    path(const std::string& path);
+
+    path(const path& rhs) = default;
+    path(path&& rhs) = default;
+
+    path& operator=(const path& rhs) = default;
+    path& operator=(path&& rhs) = default;
+
+    bool empty();
+
+    private:
+        std::vector<std::string> names;
+};
+
+#endif
---
+++ kernel/src/path.cpp
@@ -0,0 +1,20 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2016.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include "vfs/path.hpp"
+
+path::path(const std::string& path){
+    auto parts = std::split(path, '/');
+    names.reserve(parts.size());
+    for(auto& part : parts){
+        names.push_back(part);
+    }
+}
+
+bool path::empty(){
+    return names.empty();
+}
