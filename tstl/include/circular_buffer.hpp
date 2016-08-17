f0ca114e056b743b034009e06e4fb9ba7f8608ad
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-17 20:34:03 +0200

    Some doc

--- tstl/include/circular_buffer.hpp
+++ tstl/include/circular_buffer.hpp
@@ -8,6 +8,12 @@
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H
 
+/*!
+ * \brief A circular buffer of maximum size S
+ *
+ * In a circular buffer, push are done on one side of the queue and
+ * pop on the other side (FIFO)
+ */
 template<typename T, size_t S>
 struct circular_buffer {
 private:
@@ -19,18 +25,31 @@ private:
     volatile size_t end;
 
 public:
+    /*!
+     * \brief Construct a new circular_buffer
+     */
     circular_buffer() : start(0), end(0) {
         //Nothing to init
     }
 
+    /*!
+     * \brief Returns true if the buffer is full
+     */
     bool full() const {
         return (end + 1) % Size == start;
     }
 
+    /*!
+     * \brief Returns true if the buffer is empty
+     */
     bool empty() const {
         return end == start;
     }
 
+    /*!
+     * \brief Push the given value to the buffer
+     * \param value The value
+     */
     bool push(T value){
         if(full()){
             return false;
@@ -42,16 +61,28 @@ public:
         }
     }
 
+    /*!
+     * \brief Returns the value at the top of the buffer
+     */
     T top() const {
         return buffer[start];
     }
 
+    /*!
+     * \brief Removes and returns the value at the top of the buffer
+     */
     T pop(){
         auto value = buffer[start];
         start = (start + 1) % Size;
         return value;
     }
 
+    /*!
+     * \brief Removes the last element that was pushed
+     *
+     * This should only be used if the buffer is used by a single
+     * thread.
+     */
     void pop_last(){
         if(end == 0){
             end = Size - 1;
