1dae04af2bcaa740064a2ddb2064183a0b268036
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-17 21:57:22 +0200

    Cleanup

--- programs/odin/src/main.cpp
+++ programs/odin/src/main.cpp
@@ -4,6 +4,7 @@
 // (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
 //=======================================================================
+
 #include <system.hpp>
 #include <graphics.hpp>
 #include <print.hpp>
