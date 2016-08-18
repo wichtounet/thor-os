e1ab6905d8d26dbb090b9830533f2529cb5b2ad1
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-18 20:01:05 +0200

    Test more

--- programs/keyboard/src/main.cpp
+++ programs/keyboard/src/main.cpp
@@ -12,10 +12,20 @@ const char* source = "Hello world";
 int main(){
     char buffer[16];
 
-    for(int i = 0; i < 10; ++i){
-        auto c = read_input(buffer, 15);
+    printf("Read 1 string (max 15)\n");
+
+    auto c = read_input(buffer, 15);
+    buffer[c] = '\0';
+    print(buffer);
+
+    printf("Read 1 string (max 15) with timeout 5\n");
+    c = read_input(buffer, 15, 5000);
+
+    if(c){
         buffer[c] = '\0';
         print(buffer);
+    } else {
+        printf("Timeout reached\n");
     }
 
     return 0;
