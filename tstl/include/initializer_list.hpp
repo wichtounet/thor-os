860bb1618d7335e6d2d62861bbf5a2e920432a05
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-03 23:27:08 +0200

    Cleanup

--- tstl/include/initializer_list.hpp
+++ tstl/include/initializer_list.hpp
@@ -24,8 +24,8 @@ public:
     using reference       = const T&;
     using const_reference = const T&;
     using size_type       = size_t;
-    using iterator       = const T*;
-    using const_iterator = const T*;
+    using iterator        = const T*;
+    using const_iterator  = const T*;
 
     constexpr initializer_list() noexcept : first(nullptr), _size(0) {
         // Nothing else to init
@@ -45,13 +45,13 @@ public:
 };
 
 template <typename T>
-inline constexpr const T* begin(initializer_list<T> __il) noexcept {
-    return __il.begin();
+inline constexpr const T* begin(initializer_list<T> list) noexcept {
+    return list.begin();
 }
 
 template <typename T>
-inline constexpr const T* end(initializer_list<T> __il) noexcept {
-    return __il.end();
+inline constexpr const T* end(initializer_list<T> list) noexcept {
+    return list.end();
 }
 
 } //end of namespace std
