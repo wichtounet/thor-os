6841823de902152a5e41b5d02d3f54a53105f6ea
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-04 11:09:09 +0200

    Implement back_inserter and front_inserter

--- tstl/include/iterator.hpp
+++ tstl/include/iterator.hpp
@@ -21,6 +21,90 @@ size_t distance(Iterator it, Iterator end){
     return end - it;
 }
 
+template< typename Container >
+struct back_insert_iterator {
+    using container_type = Container;
+    using value_type = void;
+    using difference_type = void;
+    using reference = void;
+    using const_reference = void;
+
+    container_type& container;
+
+    explicit back_insert_iterator(container_type& container) : container(container) {}
+
+    back_insert_iterator& operator=(const typename container_type::value_type& value){
+        container.push_back(value);
+
+        return *this;
+    }
+
+    back_insert_iterator& operator=(typename container_type::value_type&& value){
+        container.push_back(std::move(value));
+
+        return *this;
+    }
+
+    back_insert_iterator& operator*(){
+        return *this;
+    }
+
+    back_insert_iterator& operator++(){
+        return *this;
+    }
+
+    back_insert_iterator& operator++(int){
+        return *this;
+    }
+};
+
+template< typename Container >
+struct front_insert_iterator {
+    using container_type = Container;
+    using value_type = void;
+    using difference_type = void;
+    using reference = void;
+    using const_reference = void;
+
+    container_type& container;
+
+    explicit front_insert_iterator(container_type& container) : container(container) {}
+
+    front_insert_iterator& operator=(const typename container_type::value_type& value){
+        container.push_front(value);
+
+        return *this;
+    }
+
+    front_insert_iterator& operator=(typename container_type::value_type&& value){
+        container.push_front(std::move(value));
+
+        return *this;
+    }
+
+    front_insert_iterator& operator*(){
+        return *this;
+    }
+
+    front_insert_iterator& operator++(){
+        return *this;
+    }
+
+    front_insert_iterator& operator++(int){
+        return *this;
+    }
+};
+
+template <typename Container>
+std::back_insert_iterator<Container> back_inserter(Container& c) {
+    return std::back_insert_iterator<Container>(c);
+}
+
+template <typename Container>
+std::front_insert_iterator<Container> front_inserter(Container& c) {
+    return std::front_insert_iterator<Container>(c);
+}
+
 } //end of namespace std
 
 #endif
