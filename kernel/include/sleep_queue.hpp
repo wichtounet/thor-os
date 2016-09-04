af9248e43bb2ea7d7ddfd668cdf68689dc961316
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-04 21:16:13 +0200

    Refactor sleep_queue

--- kernel/include/sleep_queue.hpp
+++ kernel/include/sleep_queue.hpp
@@ -12,9 +12,7 @@
 #include <lock_guard.hpp>
 
 #include "spinlock.hpp"
-#include "scheduler.hpp"
-#include "logging.hpp"
-#include "assert.hpp"
+#include "process.hpp"
 
 /*!
  * \brief A simple sleep queue
@@ -28,98 +26,29 @@ public:
     /*!
      * \brief Test if the sleep queue is empty
      */
-    bool empty() const {
-        std::lock_guard<spinlock> l(lock);
-
-        return queue.empty();
-    }
+    bool empty() const ;
 
     /*!
      * \brief Returns the top process in the queue.
      *
      * If the queue is empty, the behaviour is undefined.
      */
-    scheduler::pid_t top_process() const {
-        std::lock_guard<spinlock> l(lock);
-
-        return queue.top();
-    }
+    scheduler::pid_t top_process() const ;
 
     /*!
      * \brief Wake up the first process from the queue.
      */
-    scheduler::pid_t wake_up(){
-        std::lock_guard<spinlock> l(lock);
-
-        while(!queue.empty()){
-            // Get the first process
-            auto pid = queue.top();
-
-            // Remove it
-            queue.pop();
-
-            if(pid != scheduler::INVALID_PID){
-                logging::logf(logging::log_level::TRACE, "sleep_queue: wake %d\n", pid);
-
-                // Indicate to the scheduler that this process will be able to run
-                // We use a hint here because it is possible that the thread was
-                // already woken up from sleep
-                scheduler::unblock_process_hint(pid);
-
-                return pid;
-            }
-        }
-
-        return scheduler::INVALID_PID;
-    }
+    scheduler::pid_t wake_up();
 
     /*!
      * \brief Wake up all the processes from the queue.
      */
-    void wake_up_all(){
-        std::lock_guard<spinlock> l(lock);
-
-        while(!queue.empty()){
-            // Get the first process
-            auto pid = queue.top();
-
-            // Remove it
-            queue.pop();
-
-            if(pid != scheduler::INVALID_PID){
-                logging::logf(logging::log_level::TRACE, "sleep_queue: wake(all) %d\n", pid);
-
-                // Indicate to the scheduler that this process will be able to run
-                // We use a hint here because it is possible that the thread was
-                // already woken up from sleep
-                scheduler::unblock_process_hint(pid);
-            }
-        }
-    }
+    void wake_up_all();
 
     /*!
      * \brief Wait inside the queue until woken up.
      */
-    void sleep(){
-        lock.acquire();
-
-        //Get the current process information
-        auto pid = scheduler::get_pid();
-
-        logging::logf(logging::log_level::TRACE, "sleep_queue: wait %d\n", pid);
-
-        //Enqueue the process in the sleep queue
-        queue.push(pid);
-
-        thor_assert(!queue.full(), "The sleep_queue queue is full!");
-
-        //This process will sleep
-        scheduler::block_process_light(pid);
-
-        lock.release();
-
-        scheduler::reschedule();
-    }
+    void sleep();
 
     /*!
      * \brief Wait inside the queue until woken up or until the
@@ -127,57 +56,7 @@ public:
      *
      * \return true if the thread was woken up, false if the timeout is passed
      */
-    bool sleep(size_t ms){
-        if(!ms){
-            return false;
-        }
-
-        lock.acquire();
-
-        //Get the current process information
-        auto pid = scheduler::get_pid();
-
-        logging::logf(logging::log_level::TRACE, "sleep_queue: %u wait with timeout %u\n", pid, ms);
-
-        //Enqueue the process in the sleep queue
-        queue.push(pid);
-
-        thor_assert(!queue.full(), "The sleep_queue queue is full!");
-
-        //This process will sleep
-        scheduler::block_process_timeout_light(pid, ms);
-
-        lock.release();
-
-        scheduler::reschedule();
-
-        // At this point we need the lock again to check the queue
-        lock.acquire();
-
-        bool obtained = true;
-
-        // If the queue still contains our pid, it means a wake up
-        // from timeout
-
-        if(!queue.empty()){
-            // If the pid is on top, pop it
-            if(queue.top() == pid){
-                obtained = false;
-                queue.pop();
-            }
-            // If the pid is inside the queue, replace it with an invalid pid
-            // If this happens too often, we'll need a better pid queue
-            else if(queue.contains(pid)){
-                obtained = false;
-                queue.replace(pid, scheduler::INVALID_PID);
-            }
-        }
-
-        // Final release of the lock
-        lock.release();
-
-        return obtained;
-    }
+    bool sleep(size_t ms);
 };
 
 #endif
---
+++ kernel/src/sleep_queue.cpp
@@ -0,0 +1,142 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2016.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#include "sleep_queue.hpp"
+#include "scheduler.hpp"
+#include "logging.hpp"
+#include "assert.hpp"
+
+bool sleep_queue::empty() const {
+    std::lock_guard<spinlock> l(lock);
+
+    return queue.empty();
+}
+
+scheduler::pid_t sleep_queue::top_process() const {
+    std::lock_guard<spinlock> l(lock);
+
+    return queue.top();
+}
+
+scheduler::pid_t sleep_queue::wake_up() {
+    std::lock_guard<spinlock> l(lock);
+
+    while (!queue.empty()) {
+        // Get the first process
+        auto pid = queue.top();
+
+        // Remove it
+        queue.pop();
+
+        if (pid != scheduler::INVALID_PID) {
+            logging::logf(logging::log_level::TRACE, "sleep_queue: wake %d\n", pid);
+
+            // Indicate to the scheduler that this process will be able to run
+            // We use a hint here because it is possible that the thread was
+            // already woken up from sleep
+            scheduler::unblock_process_hint(pid);
+
+            return pid;
+        }
+    }
+
+    return scheduler::INVALID_PID;
+}
+
+void sleep_queue::wake_up_all() {
+    std::lock_guard<spinlock> l(lock);
+
+    while (!queue.empty()) {
+        // Get the first process
+        auto pid = queue.top();
+
+        // Remove it
+        queue.pop();
+
+        if (pid != scheduler::INVALID_PID) {
+            logging::logf(logging::log_level::TRACE, "sleep_queue: wake(all) %d\n", pid);
+
+            // Indicate to the scheduler that this process will be able to run
+            // We use a hint here because it is possible that the thread was
+            // already woken up from sleep
+            scheduler::unblock_process_hint(pid);
+        }
+    }
+}
+
+void sleep_queue::sleep() {
+    lock.acquire();
+
+    //Get the current process information
+    auto pid = scheduler::get_pid();
+
+    logging::logf(logging::log_level::TRACE, "sleep_queue: wait %d\n", pid);
+
+    //Enqueue the process in the sleep queue
+    queue.push(pid);
+
+    thor_assert(!queue.full(), "The sleep_queue queue is full!");
+
+    //This process will sleep
+    scheduler::block_process_light(pid);
+
+    lock.release();
+
+    scheduler::reschedule();
+}
+
+bool sleep_queue::sleep(size_t ms) {
+    if (!ms) {
+        return false;
+    }
+
+    lock.acquire();
+
+    //Get the current process information
+    auto pid = scheduler::get_pid();
+
+    logging::logf(logging::log_level::TRACE, "sleep_queue: %u wait with timeout %u\n", pid, ms);
+
+    //Enqueue the process in the sleep queue
+    queue.push(pid);
+
+    thor_assert(!queue.full(), "The sleep_queue queue is full!");
+
+    //This process will sleep
+    scheduler::block_process_timeout_light(pid, ms);
+
+    lock.release();
+
+    scheduler::reschedule();
+
+    // At this point we need the lock again to check the queue
+    lock.acquire();
+
+    bool obtained = true;
+
+    // If the queue still contains our pid, it means a wake up
+    // from timeout
+
+    if (!queue.empty()) {
+        // If the pid is on top, pop it
+        if (queue.top() == pid) {
+            obtained = false;
+            queue.pop();
+        }
+        // If the pid is inside the queue, replace it with an invalid pid
+        // If this happens too often, we'll need a better pid queue
+        else if (queue.contains(pid)) {
+            obtained = false;
+            queue.replace(pid, scheduler::INVALID_PID);
+        }
+    }
+
+    // Final release of the lock
+    lock.release();
+
+    return obtained;
+}
