091edbcd4adc40948989881d4a71eceb69c64e1a
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-09 17:08:25 +0200

    Fix the indentation

--- kernel/src/drivers/pci.cpp
+++ kernel/src/drivers/pci.cpp
@@ -19,94 +19,94 @@ std::vector<pci::device_descriptor> devices;
 #define PCI_CONFIG_ADDRESS 0xCF8
 #define PCI_CONFIG_DATA 0xCFC
 
- uint16_t get_vendor_id(uint8_t bus, uint8_t device, uint8_t function){
-     // read "device id | vendor id"
-     auto large = pci::read_config_dword(bus, device, function, 0);
-     // extract vendor id
-     return large;
- }
+uint16_t get_vendor_id(uint8_t bus, uint8_t device, uint8_t function){
+    // read "device id | vendor id"
+    auto large = pci::read_config_dword(bus, device, function, 0);
+    // extract vendor id
+    return large;
+}
 
- uint16_t get_device_id(uint8_t bus, uint8_t device, uint8_t function){
-     // read "device id | vendor id"
-     auto large = pci::read_config_dword(bus, device, function, 0);
-     // extract device id
-     return large >> 16;
- }
+uint16_t get_device_id(uint8_t bus, uint8_t device, uint8_t function){
+    // read "device id | vendor id"
+    auto large = pci::read_config_dword(bus, device, function, 0);
+    // extract device id
+    return large >> 16;
+}
 
- uint8_t get_class_code(uint8_t bus, uint8_t device, uint8_t function){
-     // read "class code | subclass | prog if | revision id"
-     auto large = pci::read_config_dword(bus, device, function, 8);
-     // extract class code only
-     return large >> 24 & 0xFF;
- }
+uint8_t get_class_code(uint8_t bus, uint8_t device, uint8_t function){
+    // read "class code | subclass | prog if | revision id"
+    auto large = pci::read_config_dword(bus, device, function, 8);
+    // extract class code only
+    return large >> 24 & 0xFF;
+}
 
- uint8_t get_subclass(uint8_t bus, uint8_t device, uint8_t function){
-     // read "class code | subclass | prog if | revision id"
-     auto large = pci::read_config_dword(bus, device, function, 8);
-     // extract subclass only
-     return large >> 16 & 0xFF;
- }
+uint8_t get_subclass(uint8_t bus, uint8_t device, uint8_t function){
+    // read "class code | subclass | prog if | revision id"
+    auto large = pci::read_config_dword(bus, device, function, 8);
+    // extract subclass only
+    return large >> 16 & 0xFF;
+}
 
- uint8_t get_header_type(uint8_t bus, uint8_t device, uint8_t function){
-     // read "BIST | header type | latency timer | cache line size"
-     auto large = pci::read_config_dword(bus, device, function, 12);
-     // extract header type only
-     return large >> 16 & 0xFF;
- }
+uint8_t get_header_type(uint8_t bus, uint8_t device, uint8_t function){
+    // read "BIST | header type | latency timer | cache line size"
+    auto large = pci::read_config_dword(bus, device, function, 12);
+    // extract header type only
+    return large >> 16 & 0xFF;
+}
 
- void check_function(uint8_t bus, uint8_t device, uint8_t function){
-     auto vendor_id = get_vendor_id(bus, device, function);
-     if(vendor_id == 0xFFFF) {
-         return;
-     }
+void check_function(uint8_t bus, uint8_t device, uint8_t function){
+    auto vendor_id = get_vendor_id(bus, device, function);
+    if(vendor_id == 0xFFFF) {
+        return;
+    }
 
-     auto device_id = get_device_id(bus, device, function);
-     auto class_code = get_class_code(bus, device, function);
-     auto sub_class = get_subclass(bus, device, function);
+    auto device_id = get_device_id(bus, device, function);
+    auto class_code = get_class_code(bus, device, function);
+    auto sub_class = get_subclass(bus, device, function);
 
-     logging::logf(logging::log_level::DEBUG, "Found device pci:%u:%u:%u (vendor:%u class:%u subclass:%u) \n",
-         uint64_t(bus), uint64_t(device), uint64_t(function), uint64_t(vendor_id), uint64_t(class_code), uint64_t(sub_class));
+    logging::logf(logging::log_level::DEBUG, "Found device pci:%u:%u:%u (vendor:%u class:%u subclass:%u) \n",
+        uint64_t(bus), uint64_t(device), uint64_t(function), uint64_t(vendor_id), uint64_t(class_code), uint64_t(sub_class));
 
-     devices.emplace_back();
+    devices.emplace_back();
 
-     pci::device_descriptor& device_desc = devices.back();
+    pci::device_descriptor& device_desc = devices.back();
 
-     device_desc.bus = bus;
-     device_desc.device = device;
-     device_desc.function = function;
-     device_desc.vendor_id = vendor_id;
-     device_desc.device_id = device_id;
-     device_desc.class_code = class_code;
-     device_desc.sub_class = sub_class;
+    device_desc.bus = bus;
+    device_desc.device = device;
+    device_desc.function = function;
+    device_desc.vendor_id = vendor_id;
+    device_desc.device_id = device_id;
+    device_desc.class_code = class_code;
+    device_desc.sub_class = sub_class;
 
-     if(class_code < static_cast<uint8_t>(pci::device_class_type::RESERVED)){
+    if(class_code < static_cast<uint8_t>(pci::device_class_type::RESERVED)){
         device_desc.class_type = static_cast<pci::device_class_type>(class_code);
-     } else if(class_code < static_cast<uint8_t>(pci::device_class_type::UNKNOWN)){
+    } else if(class_code < static_cast<uint8_t>(pci::device_class_type::UNKNOWN)){
         device_desc.class_type = pci::device_class_type::RESERVED;
-     } else {
+    } else {
         device_desc.class_type = pci::device_class_type::UNKNOWN;
-     }
+    }
 
-     std::string pci_name = "pci:" + std::to_string(bus) + ':' + std::to_string(device) + ':' + std::to_string(function);
+    std::string pci_name = "pci:" + std::to_string(bus) + ':' + std::to_string(device) + ':' + std::to_string(function);
 
-     auto p = path("/pci") / pci_name;
+    auto p = path("/pci") / pci_name;
 
-     sysfs::set_constant_value(path("/sys"), p / "vendor", std::to_string(vendor_id));
-     sysfs::set_constant_value(path("/sys"), p / "device", std::to_string(device_id));
-     sysfs::set_constant_value(path("/sys"), p / "class", std::to_string(class_code));
-     sysfs::set_constant_value(path("/sys"), p / "subclass", std::to_string(sub_class));
- }
+    sysfs::set_constant_value(path("/sys"), p / "vendor", std::to_string(vendor_id));
+    sysfs::set_constant_value(path("/sys"), p / "device", std::to_string(device_id));
+    sysfs::set_constant_value(path("/sys"), p / "class", std::to_string(class_code));
+    sysfs::set_constant_value(path("/sys"), p / "subclass", std::to_string(sub_class));
+}
 
- void check_device(uint8_t bus, uint8_t device) {
-     check_function(bus, device, 0);
+void check_device(uint8_t bus, uint8_t device) {
+    check_function(bus, device, 0);
 
-     auto header_type = get_header_type(bus, device, 0);
-     if((header_type & 0x80) != 0){
-         for(uint8_t function = 1; function < 8; ++function){
-             check_function(bus, device, function);
-         }
-     }
- }
+    auto header_type = get_header_type(bus, device, 0);
+    if((header_type & 0x80) != 0){
+        for(uint8_t function = 1; function < 8; ++function){
+            check_function(bus, device, function);
+        }
+    }
+}
 
 void brute_force_check_all_buses(){
     for(uint16_t bus = 0; bus < 256; ++bus) {
