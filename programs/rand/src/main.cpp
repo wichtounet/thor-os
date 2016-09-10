89997be334d81eb25566688ba0077b2d2bab6031
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-10 18:36:11 +0200

    Simple program to generate random numbers

---
+++ programs/rand/src/main.cpp
@@ -0,0 +1,20 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2016.
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://www.opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <random.hpp>
+
+#include <tlib/system.hpp>
+#include <tlib/print.hpp>
+
+int main(int argc, char* argv[]){
+    std::default_random_engine engine(tlib::ms_time());
+    std::uniform_int_distribution<> dist(0, 100);
+
+    tlib::printf("%d\n", dist(engine));
+
+    return 0;
+}
