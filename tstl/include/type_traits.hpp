9de8550b4e5ddb5d755e44894d686ce35a4d0138
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-14 20:00:12 +0200

    Support for std::add_const

--- tstl/include/type_traits.hpp
+++ tstl/include/type_traits.hpp
@@ -73,6 +73,21 @@ struct remove_const<const T> {
     typedef T type;
 };
 
+/* add_const */
+
+template<typename T>
+struct add_const {
+    using type = const T;
+};
+
+template<typename T>
+struct add_const<const T> {
+    using type = T;
+};
+
+template<typename T>
+using add_const_t = typename add_const<T>::type;
+
 /* remove_volatile */
 
 template<class T>
