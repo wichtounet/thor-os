7906a4c9a932f70938ebf0fbcf4673ba0d1ab847
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-17 19:45:20 +0200

    Fix the CI branch

--- ci/Jenkins.groovy
+++ ci/Jenkins.groovy
@@ -1,6 +1,6 @@
 node {
    stage 'git'
-   checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/wichtounet/thor-os.git']]])
+   checkout([$class: 'GitSCM', branches: [[name: '*/develop']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/wichtounet/thor-os.git']]])
 
    stage 'pre-analysis'
    sh 'cppcheck --xml-version=2 --enable=all --std=c++11 kernel/src kernel/include tstl/include tstl/test_suite tlib/include tlib/src  printf/include tlib/include tlib/src 2> cppcheck_report.xml'
