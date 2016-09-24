131a95c1fc754f503647039eb031190ce650c014
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-24 15:43:50 +0200

    Reformat

--- kernel/src/vesa_console.cpp
+++ kernel/src/vesa_console.cpp
@@ -11,11 +11,12 @@
 
 namespace {
 
-constexpr const size_t MARGIN = 10;
+constexpr const size_t MARGIN  = 10;
 constexpr const size_t PADDING = 5;
-constexpr const size_t LEFT = MARGIN + PADDING;
-constexpr const size_t TOP = 40;
+constexpr const size_t LEFT    = MARGIN + PADDING;
+constexpr const size_t TOP     = 40;
 
+// Constants extracted from VESA
 size_t _lines;
 size_t _columns;
 uint32_t _color;
@@ -23,12 +24,12 @@ size_t buffer_size;
 
 } //end of anonymous namespace
 
-void vesa_console::init(){
+void vesa_console::init() {
     auto& block = *reinterpret_cast<vesa::mode_info_block_t*>(early::vesa_mode_info_address);
 
     _columns = (block.width - (MARGIN + PADDING) * 2) / 8;
-    _lines = (block.height - TOP - MARGIN - PADDING) / 16;
-    _color = vesa::make_color(0, 255, 0);
+    _lines   = (block.height - TOP - MARGIN - PADDING) / 16;
+    _color   = vesa::make_color(0, 255, 0);
 
     buffer_size = block.height * block.bytes_per_scan_line;
 
@@ -46,30 +47,30 @@ void vesa_console::init(){
     vesa::draw_char(title_left + 24, PADDING + MARGIN, 'R', _color);
 }
 
-size_t vesa_console::lines(){
+size_t vesa_console::lines() {
     return _lines;
 }
 
-size_t vesa_console::columns(){
+size_t vesa_console::columns() {
     return _columns;
 }
 
-void vesa_console::clear(){
-    vesa::draw_rect(LEFT, TOP, _columns * 8, _lines* 16, 0, 0, 0);
+void vesa_console::clear() {
+    vesa::draw_rect(LEFT, TOP, _columns * 8, _lines * 16, 0, 0, 0);
 }
 
-void vesa_console::scroll_up(){
+void vesa_console::scroll_up() {
     vesa::move_lines_up(TOP + 16, LEFT, _columns * 8, (_lines - 1) * 16, 16);
     vesa::draw_rect(LEFT, TOP + (_lines - 1) * 16, _columns * 8, 16, 0, 0, 0);
 }
 
-void vesa_console::print_char(size_t line, size_t column, char c){
+void vesa_console::print_char(size_t line, size_t column, char c) {
     vesa::draw_char(LEFT + 8 * column, TOP + 16 * line, c, _color);
 }
 
-void* vesa_console::save(void* buffer){
+void* vesa_console::save(void* buffer) {
     void* buffer32 = static_cast<uint32_t*>(buffer);
-    if(!buffer32){
+    if (!buffer32) {
         buffer32 = new uint32_t[buffer_size];
     }
 
@@ -78,6 +79,6 @@ void* vesa_console::save(void* buffer){
     return buffer32;
 }
 
-void vesa_console::restore(void* buffer){
+void vesa_console::restore(void* buffer) {
     vesa::redraw(static_cast<char*>(buffer));
 }
