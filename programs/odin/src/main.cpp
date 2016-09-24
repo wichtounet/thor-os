5ad48998975075a5e8522e9949162bd302b0181b
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-24 19:56:52 +0200

    Reformat code

--- programs/odin/src/main.cpp
+++ programs/odin/src/main.cpp
@@ -19,25 +19,25 @@ namespace {
 
 uint32_t* z_buffer = nullptr;
 
-uint64_t width = 0;
-uint64_t height = 0;
-uint64_t x_shift = 0;
-uint64_t y_shift = 0;
+uint64_t width               = 0;
+uint64_t height              = 0;
+uint64_t x_shift             = 0;
+uint64_t y_shift             = 0;
 uint64_t bytes_per_scan_line = 0;
-uint64_t red_shift = 0;
-uint64_t green_shift = 0;
-uint64_t blue_shift = 0;
+uint64_t red_shift           = 0;
+uint64_t green_shift         = 0;
+uint64_t blue_shift          = 0;
 
 #include <tlib/Liberation.inl>
 
-uint32_t make_color(uint8_t r, uint8_t g, uint8_t b){
+uint32_t make_color(uint8_t r, uint8_t g, uint8_t b) {
     return (r << red_shift) + (g << green_shift) + (b << blue_shift);
 }
 
-void fill_buffer(uint32_t color){
+void fill_buffer(uint32_t color) {
     auto where = 0;
-    for(size_t j = 0; j < height; ++j){
-        for(size_t i = 0; i < width; ++i){
+    for (size_t j = 0; j < height; ++j) {
+        for (size_t i = 0; i < width; ++i) {
             z_buffer[where + i] = color;
         }
 
@@ -45,32 +45,32 @@ void fill_buffer(uint32_t color){
     }
 }
 
-void draw_pixel(size_t x, size_t y, uint32_t color){
+void draw_pixel(size_t x, size_t y, uint32_t color) {
     z_buffer[x + y * y_shift] = color;
 }
 
-void draw_hline(size_t x, size_t y, size_t w, uint32_t color){
+void draw_hline(size_t x, size_t y, size_t w, uint32_t color) {
     auto where = x + y * y_shift;
 
-    for(size_t i = 0; i < w; ++i){
+    for (size_t i = 0; i < w; ++i) {
         z_buffer[where + i] = color;
     }
 }
 
-void draw_vline(size_t x, size_t y, size_t h, uint32_t color){
+void draw_vline(size_t x, size_t y, size_t h, uint32_t color) {
     auto where = x + y * y_shift;
 
-    for(size_t j = 0; j < h; ++j){
+    for (size_t j = 0; j < h; ++j) {
         z_buffer[where] = color;
         where += y_shift;
     }
 }
 
-void draw_rect(size_t x, size_t y, size_t w, size_t h, uint32_t color){
+void draw_rect(size_t x, size_t y, size_t w, size_t h, uint32_t color) {
     auto where = x + y * y_shift;
 
-    for(size_t j = 0; j < h; ++j){
-        for(size_t i = 0; i < w; ++i){
+    for (size_t j = 0; j < h; ++j) {
+        for (size_t i = 0; i < w; ++i) {
             z_buffer[where + i] = color;
         }
 
@@ -78,15 +78,15 @@ void draw_rect(size_t x, size_t y, size_t w, size_t h, uint32_t color){
     }
 }
 
-void draw_char(size_t x, size_t y, char c, uint32_t color){
+void draw_char(size_t x, size_t y, char c, uint32_t color) {
     auto where = x + y * y_shift;
 
     auto font_char = &Liberation_VESA_data[c * 16];
 
-    for(size_t i = 0; i < 16; ++i){
-        for(size_t j = 0; j < 8; ++j){
-            if(font_char[i] & (1 << (8 - j))){
-                z_buffer[where+j] = color;
+    for (size_t i = 0; i < 16; ++i) {
+        for (size_t j = 0; j < 8; ++j) {
+            if (font_char[i] & (1 << (8 - j))) {
+                z_buffer[where + j] = color;
             }
         }
 
@@ -94,33 +94,33 @@ void draw_char(size_t x, size_t y, char c, uint32_t color){
     }
 }
 
-void draw_string(size_t x, size_t y, const char* s, uint32_t color){
-    while(*s){
+void draw_string(size_t x, size_t y, const char* s, uint32_t color) {
+    while (*s) {
         draw_char(x, y, *s, color);
         ++s;
         x += 8;
     }
 }
 
-void paint_cursor(){
+void paint_cursor() {
     auto x = tlib::graphics::mouse_x();
     auto y = tlib::graphics::mouse_y();
 
     auto color = make_color(20, 20, 20);
 
     draw_pixel(x, y, color);
-    draw_hline(x, y+1, 2, color);
-    draw_hline(x, y+2, 3, color);
-    draw_hline(x, y+3, 4, color);
-    draw_hline(x, y+4, 5, color);
-    draw_hline(x, y+5, 4, color);
-    draw_pixel(x, y+6, color);
-    draw_hline(x+2, y+6, 2, color);
-    draw_hline(x+3, y+7, 2, color);
-    draw_hline(x+3, y+8, 2, color);
+    draw_hline(x, y + 1, 2, color);
+    draw_hline(x, y + 2, 3, color);
+    draw_hline(x, y + 3, 4, color);
+    draw_hline(x, y + 4, 5, color);
+    draw_hline(x, y + 5, 4, color);
+    draw_pixel(x, y + 6, color);
+    draw_hline(x + 2, y + 6, 2, color);
+    draw_hline(x + 3, y + 7, 2, color);
+    draw_hline(x + 3, y + 8, 2, color);
 }
 
-void paint_top_bar(){
+void paint_top_bar() {
     draw_rect(0, 0, width, 18, make_color(51, 51, 51));
     draw_rect(0, 18, width, 2, make_color(25, 25, 25));
 
@@ -142,16 +142,16 @@ private:
     uint32_t color;
     uint32_t border_color;
 
-    bool drag = false;
+    bool drag            = false;
     int64_t drag_start_x = 0;
     int64_t drag_start_y = 0;
 
 public:
-
     // TODO Relax std::vector to allow for non-default-constructible
-    window(){}
+    window() {}
 
-    window(size_t x, size_t y, size_t width, size_t height) : x(x), y(y), width(width), height(height) {
+    window(size_t x, size_t y, size_t width, size_t height)
+            : x(x), y(y), width(width), height(height) {
         std::default_random_engine eng(tlib::ms_time());
         std::uniform_int_distribution<> color_dist(0, 255);
 
@@ -165,15 +165,15 @@ public:
     window& operator=(const window& rhs) = default;
     window& operator=(window&& rhs) = default;
 
-    void update(){
-        if(drag){
+    void update() {
+        if (drag) {
             auto mouse_x = tlib::graphics::mouse_x();
             auto mouse_y = tlib::graphics::mouse_y();
 
             auto delta_x = int64_t(mouse_x) - drag_start_x;
             auto delta_y = int64_t(mouse_y) - drag_start_y;
 
-            if(int64_t(x) + delta_x < 0){
+            if (int64_t(x) + delta_x < 0) {
                 x = 0;
             } else {
                 x += delta_x;
@@ -181,7 +181,7 @@ public:
                 x = std::min(x, ::width - width);
             }
 
-            if(int64_t(y) + delta_y < 20){
+            if (int64_t(y) + delta_y < 20) {
                 y = 20;
             } else {
                 y += delta_y;
@@ -198,7 +198,7 @@ public:
         constexpr const size_t border = 2;
 
         // Draw the background of the window
-        draw_rect(x+2, y+2, width-4, height-4, color);
+        draw_rect(x + 2, y + 2, width - 4, height - 4, color);
 
         // Draw the outer border
         draw_rect(x, y, width, border, border_color);
@@ -210,42 +210,42 @@ public:
         draw_rect(x, y + 18, width, border, border_color);
     }
 
-    bool mouse_in_title(){
+    bool mouse_in_title() {
         auto mouse_x = tlib::graphics::mouse_x();
         auto mouse_y = tlib::graphics::mouse_y();
 
         return mouse_x >= x && mouse_x <= x + width && mouse_y >= y + 2 && mouse_y <= mouse_y + 18;
     }
 
-    bool inside(size_t look_x, size_t look_y){
+    bool inside(size_t look_x, size_t look_y) {
         return look_x >= x && look_x <= x + width && look_y >= y && look_y <= y + height;
     }
 
-    void start_drag(){
-        if(!drag){
-            drag = true;
+    void start_drag() {
+        if (!drag) {
+            drag         = true;
             drag_start_x = tlib::graphics::mouse_x();
             drag_start_y = tlib::graphics::mouse_y();
         }
     }
 
-    void stop_drag(){
+    void stop_drag() {
         drag = false;
     }
 };
 
 std::vector<window> windows;
 
-void raise(){
+void raise() {
     auto mouse_x = tlib::graphics::mouse_x();
     auto mouse_y = tlib::graphics::mouse_y();
 
-    for(auto it = windows.begin(); it != windows.end(); ++it){
+    for (auto it = windows.begin(); it != windows.end(); ++it) {
         auto& window = *it;
 
-        if(window.inside(mouse_x, mouse_y)){
+        if (window.inside(mouse_x, mouse_y)) {
             //If the window is not the first, we raise it
-            if(it != windows.begin()){
+            if (it != windows.begin()) {
                 auto copy = window;
                 windows.erase(it);
                 windows.push_front(copy);
@@ -258,15 +258,15 @@ void raise(){
 
 } // end of anonnymous namespace
 
-int main(int /*argc*/, char* /*argv*/[]){
-    width = tlib::graphics::get_width();
-    height = tlib::graphics::get_height();
-    x_shift = tlib::graphics::get_x_shift();
-    y_shift = tlib::graphics::get_y_shift();
+int main(int /*argc*/, char* /*argv*/ []) {
+    width               = tlib::graphics::get_width();
+    height              = tlib::graphics::get_height();
+    x_shift             = tlib::graphics::get_x_shift();
+    y_shift             = tlib::graphics::get_y_shift();
     bytes_per_scan_line = tlib::graphics::get_bytes_per_scan_line();
-    red_shift = tlib::graphics::get_red_shift();
-    green_shift = tlib::graphics::get_green_shift();
-    blue_shift = tlib::graphics::get_blue_shift();
+    red_shift           = tlib::graphics::get_red_shift();
+    green_shift         = tlib::graphics::get_green_shift();
+    blue_shift          = tlib::graphics::get_blue_shift();
 
     size_t total_size = height * bytes_per_scan_line;
 
@@ -286,12 +286,12 @@ int main(int /*argc*/, char* /*argv*/[]){
     std::uniform_int_distribution<> height_dist(100, 250);
     std::uniform_int_distribution<> position_dist(0, 500);
 
-    while(true){
+    while (true) {
         fill_buffer(background);
 
         paint_top_bar();
 
-        for(auto& window : windows){
+        for (auto& window : windows) {
             window.update();
         }
 
@@ -305,11 +305,11 @@ int main(int /*argc*/, char* /*argv*/[]){
         tlib::graphics::redraw(buffer);
 
         auto before = tlib::ms_time();
-        auto code = tlib::read_input_raw(sleep_timeout);
-        auto after = tlib::ms_time();
+        auto code   = tlib::read_input_raw(sleep_timeout);
+        auto after  = tlib::ms_time();
 
-        if(code != std::keycode::TIMEOUT){
-            switch(code){
+        if (code != std::keycode::TIMEOUT) {
+            switch (code) {
                 case std::keycode::RELEASED_ENTER: {
                     size_t width  = width_dist(eng);
                     size_t height = width_dist(eng);
@@ -326,7 +326,7 @@ int main(int /*argc*/, char* /*argv*/[]){
 
                     raise();
 
-                    if(windows.front().mouse_in_title()){
+                    if (windows.front().mouse_in_title()) {
                         windows.front().start_drag();
                     }
 
@@ -359,7 +359,7 @@ int main(int /*argc*/, char* /*argv*/[]){
 
             auto duration = after - before;
 
-            if(duration < sleep_timeout){
+            if (duration < sleep_timeout) {
                 tlib::sleep_ms(sleep_timeout - duration);
             }
         }
