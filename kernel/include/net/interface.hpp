9038762e00bacd1486bf52679695f2e5dca1f4a1
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-09-29 09:18:40 +0200

    Extract interface_descriptor header

---
+++ kernel/include/net/interface.hpp
@@ -0,0 +1,76 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2013-2016.
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://www.opensource.org/licenses/MIT)
+//=======================================================================
+
+#ifndef NETWORK_INTERFACE_H
+#define NETWORK_INTERFACE_H
+
+#include <types.hpp>
+#include <string.hpp>
+#include <circular_buffer.hpp>
+#include <lock_guard.hpp>
+
+#include "conc/mutex.hpp"
+#include "conc/semaphore.hpp"
+#include "conc/deferred_unique_semaphore.hpp"
+
+#include "net/ethernet_packet.hpp"
+
+#include "tlib/net_constants.hpp"
+
+namespace network {
+
+/*!
+ * \brief Abstraction of a network interface
+ */
+struct interface_descriptor {
+    size_t id;                       ///< The interface ID
+    bool enabled;                    ///< true if the interface is enabled
+    std::string name;                ///< The name of the interface
+    std::string driver;              ///< The driver of the interface
+    size_t pci_device;               ///< The pci information
+    size_t mac_address;              ///< The inteface MAC address
+    void* driver_data = nullptr;     ///<  The driver data
+    network::ip::address ip_address; ///< The interface IP address
+    network::ip::address gateway;    ///< The interface IP gateway
+
+    size_t rx_thread_pid; ///< The pid of the rx thread
+    size_t tx_thread_pid; ///< The pid of the tx thread
+
+    size_t rx_packets_counter = 0; ///< Counter of received packets
+    size_t rx_bytes_counter   = 0; ///< Counter of received bytes
+    size_t tx_packets_counter = 0; ///< Counter of transmitted packets
+    size_t tx_bytes_counter   = 0; ///< Counter of transmitted bytes
+
+    mutable mutex tx_lock;                    ///< Mutex protecting the queues
+    mutable semaphore tx_sem;                 ///< Semaphore for transmission
+    mutable deferred_unique_semaphore rx_sem; ///< Semaphore for reception
+
+    circular_buffer<ethernet::packet, 32> rx_queue; ///< The reception queue
+    circular_buffer<ethernet::packet, 32> tx_queue; ///< The transmission queue
+
+    void (*hw_send)(interface_descriptor&, ethernet::packet& p); ///< Driver hardware send function
+
+    /*!
+     * \brief Send a packet through this interface
+     */
+    void send(ethernet::packet& p){
+        std::lock_guard<mutex> l(tx_lock);
+        tx_queue.push(p);
+        tx_sem.unlock();
+    }
+
+    /*!
+     * \brief Indicates if this function is a loopback function
+     */
+    bool is_loopback() const {
+        return driver == "loopback";
+    }
+};
+
+} // end of network namespace
+
+#endif
--- kernel/include/net/network.hpp
+++ kernel/include/net/network.hpp
@@ -9,65 +9,17 @@
 #define NETWORK_H
 
 #include <types.hpp>
-#include <string.hpp>
-#include <circular_buffer.hpp>
-#include <lock_guard.hpp>
 #include <tuple.hpp>
 #include <expected.hpp>
 
-#include <conc/mutex.hpp>
-#include <conc/semaphore.hpp>
-#include <conc/deferred_unique_semaphore.hpp>
-
 #include "tlib/net_constants.hpp"
 
+#include "net/interface.hpp"
 #include "net/ethernet_packet.hpp"
 
 namespace network {
 
 using socket_fd_t = size_t;
-
-/*!
- * \brief Abstraction of a network interface
- */
-struct interface_descriptor {
-    size_t id;                       ///< The interface ID
-    bool enabled;                    ///< true if the interface is enabled
-    std::string name;                ///< The name of the interface
-    std::string driver;              ///< The driver of the interface
-    size_t pci_device;               ///< The pci information
-    size_t mac_address;              ///< The inteface MAC address
-    void* driver_data = nullptr;     ///<  The driver data
-    network::ip::address ip_address; ///< The interface IP address
-    network::ip::address gateway;    ///< The interface IP gateway
-
-    size_t rx_thread_pid; ///< The pid of the rx thread
-    size_t tx_thread_pid; ///< The pid of the tx thread
-
-    size_t rx_packets_counter = 0;
-    size_t rx_bytes_counter   = 0;
-    size_t tx_packets_counter = 0;
-    size_t tx_bytes_counter   = 0;
-
-    mutable mutex tx_lock;                    ///< Mutex protecting the queues
-    mutable semaphore tx_sem;                 ///< Semaphore for transmission
-    mutable deferred_unique_semaphore rx_sem; ///< Semaphore for reception
-
-    circular_buffer<ethernet::packet, 32> rx_queue; ///< The reception queue
-    circular_buffer<ethernet::packet, 32> tx_queue; ///< The transmission queue
-
-    void (*hw_send)(interface_descriptor&, ethernet::packet& p);
-
-    void send(ethernet::packet& p){
-        std::lock_guard<mutex> l(tx_lock);
-        tx_queue.push(p);
-        tx_sem.unlock();
-    }
-
-    bool is_loopback() const {
-        return driver == "loopback";
-    }
-};
 
 /*!
  * \brief Early initialization of the network
