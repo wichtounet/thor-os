5854467636a7e75f33e75f38ca474d923817dcf7
Baptiste Wicht <baptiste.wicht@gmail.com>
2018-03-28 13:17:25 +0200

    Make the dynamic memory allocator more robust

--- kernel/src/kalloc.cpp
+++ kernel/src/kalloc.cpp
@@ -11,6 +11,7 @@
 #include "paging.hpp"
 #include "e820.hpp"
 #include "logging.hpp"
+#include "assert.hpp"
 
 #include "conc/int_lock.hpp"
 
@@ -18,10 +19,17 @@
 
 namespace {
 
+constexpr size_t ALIGNMENT = 16;
+
+constexpr bool aligned(size_t value){
+    return (value & (ALIGNMENT - 1)) == 0;
+}
+
 //Used to compile with malloc operations in the console
 //can produce a lot of output
-const bool DEBUG_MALLOC = false;
-const bool TRACE_MALLOC = false;
+constexpr bool DEBUG_MALLOC      = false;
+constexpr bool DEBUG_MALLOC_FULL = false;
+constexpr bool TRACE_MALLOC      = false;
 
 size_t _used_memory;
 size_t _allocations;
@@ -79,9 +87,11 @@ public:
         return _free;
     }
 
-    constexpr malloc_footer_chunk* footer() const {
+    malloc_footer_chunk* footer() const {
+        thor_assert(aligned(reinterpret_cast<uintptr_t>(static_cast<const volatile void*>(this))), "kalloc: Invalid alignment");
+
         return reinterpret_cast<malloc_footer_chunk*>(
-            reinterpret_cast<uintptr_t>(this) + __size + sizeof(malloc_header_chunk));
+            reinterpret_cast<uintptr_t>(static_cast<const volatile void*>(this)) + __size + sizeof(malloc_header_chunk));
     }
 };
 
@@ -90,7 +100,7 @@ private:
     uint64_t __size;
 
 public:
-    size_t& size(){
+    uint64_t& size(){
         return __size;
     }
 };
@@ -99,24 +109,25 @@ struct fake_head {
     uint64_t size;
     malloc_header_chunk* next;
     malloc_header_chunk* prev;
-    uint64_t size_2;
+    uint32_t _free;
+    uint16_t left;
+    uint16_t right;
+    uint64_t size_2; // This is the size of the footer
 };
 
-constexpr size_t ALIGNMENT = 16;
-
-constexpr bool aligned(size_t value){
-    return (value & (ALIGNMENT - 1)) == 0;
-}
-
-constexpr const uint64_t META_SIZE = sizeof(malloc_header_chunk) + sizeof(malloc_footer_chunk);
-constexpr const uint64_t BLOCK_SIZE = paging::PAGE_SIZE;
-constexpr const uint64_t MIN_BLOCKS = 4;
+constexpr const size_t HEADER_SIZE = sizeof(malloc_header_chunk);
+constexpr const size_t FOOTER_SIZE = sizeof(malloc_footer_chunk);
+constexpr const size_t META_SIZE = HEADER_SIZE + FOOTER_SIZE;
+constexpr const size_t BLOCK_SIZE = paging::PAGE_SIZE;
+constexpr const size_t MIN_BLOCKS = 4;
 
 constexpr const uint64_t MIN_SPLIT_BASE = ALIGNMENT == 8 ? 32 : 2 * ALIGNMENT;
 constexpr const uint64_t MIN_SPLIT = MIN_SPLIT_BASE - sizeof(malloc_footer_chunk);
 
 static_assert(aligned(sizeof(malloc_header_chunk)), "The header must be aligned");
 static_assert(aligned(MIN_SPLIT + sizeof(malloc_footer_chunk)), "The size of minimum split must guarantee alignment");
+static_assert(sizeof(malloc_footer_chunk) == sizeof(uint64_t), "The footer must a single quadword");
+static_assert(sizeof(fake_head) == META_SIZE, "The fake head must be the sum of header and footer");
 
 fake_head head;
 malloc_header_chunk* malloc_head = 0;
@@ -133,43 +144,82 @@ uint64_t* allocate_block(uint64_t blocks){
     auto virtual_memory = virtual_allocator::allocate(blocks);
 
     if(!virtual_memory){
+        // TODO Fix the leak
         return nullptr;
     }
 
     //Map the physical memory at the virtual address
-    paging::map_pages(virtual_memory, physical_memory, blocks);
+    if(!paging::map_pages(virtual_memory, physical_memory, blocks)){
+        // TODO Fix the leak
+        return nullptr;
+    }
 
     _allocated_memory += blocks * BLOCK_SIZE;
 
     return reinterpret_cast<uint64_t*>(virtual_memory);
 }
 
-template<bool Debug>
+void check_node(malloc_header_chunk* node, const char* point = nullptr){
+    if(node->size() != node->footer()->size()){
+        logging::logf(logging::log_level::ERROR, "Inconsistent header/footer size for block %p (%s)\n", node, point);
+    }
+
+    if(node->size() > 1024UL * 1024UL * 1024UL * 1024UL * 1024UL){
+        logging::logf(logging::log_level::ERROR, "Invalid header size for block %p:%u (%s)\n", node, node->size(), point);
+    }
+
+    if(node->footer()->size() > 1024UL * 1024UL * 1024UL * 1024UL * 1024UL){
+        logging::logf(logging::log_level::ERROR, "Invalid footer size for block %p:%u (%s)\n", node, node->footer()->size(), point);
+    }
+
+    if(reinterpret_cast<uintptr_t>(node->footer()) != reinterpret_cast<uintptr_t>(node) + node->size() + HEADER_SIZE){
+        logging::logf(logging::log_level::ERROR, "Inconsistent footer address for block %p\n (%s)", node, point);
+    }
+}
+
 void debug_malloc(const char* point = nullptr){
-    if(Debug){
-        if(point){
-            k_print(point);
+    if(DEBUG_MALLOC || DEBUG_MALLOC_FULL){
+        // 0. Check for inconsistencies
+
+        {
+            auto it = malloc_head;
+
+            do {
+                check_node(it, point);
+
+                it = it->next();
+            } while(it != malloc_head);
         }
 
-        auto it = malloc_head;
+        if (DEBUG_MALLOC_FULL) {
+            if(point){
+                logging::log(logging::log_level::DEBUG, point);
+            }
 
-        k_print(" next: ");
-        do {
-            printf("%u%h -> ", static_cast<size_t>(it->is_free()), reinterpret_cast<uint64_t>(it));
-            it = it->next();
-        } while(it != malloc_head);
+            {
+                auto it = malloc_head;
 
-        printf("%h\n", malloc_head);
+                logging::log("\n next: ");
+                do {
+                    logging::logf("%u:%h(%u) -> ", static_cast<size_t>(it->is_free()), reinterpret_cast<uint64_t>(it), it->size());
+                    it = it->next();
+                } while(it != malloc_head);
 
-        it = malloc_head;
+                logging::logf("%h", malloc_head);
+            }
 
-        k_print("prev: ");
-        do {
-            printf("%h <- ", reinterpret_cast<uint64_t>(it));
-            it = it->prev();
-        } while(it != malloc_head);
+            {
+                auto it = malloc_head;
 
-        printf("%h\n", malloc_head);
+                logging::log("\n prev: ");
+                do {
+                    logging::logf("%u:%h(%u) <- ", static_cast<size_t>(it->is_free()), reinterpret_cast<uint64_t>(it), it->size());
+                    it = it->prev();
+                } while(it != malloc_head);
+
+                logging::logf("%h\n", malloc_head);
+            }
+        }
     }
 }
 
@@ -215,7 +265,7 @@ void init_head(){
     malloc_head->set_right(false);
 }
 
-void expand_heap(malloc_header_chunk* current, size_t bytes = 0){
+void expand_heap(malloc_header_chunk* current, uint64_t bytes = 0){
     auto blocks = MIN_BLOCKS;
 
     if(bytes){
@@ -229,12 +279,15 @@ void expand_heap(malloc_header_chunk* current, size_t bytes = 0){
     //Allocate a new block of memory
     uint64_t* block = allocate_block(blocks);
 
+    thor_assert(block, "Impossible to allocate block for expand_heap");
+
     //Transform it into a malloc chunk
     auto header = reinterpret_cast<malloc_header_chunk*>(block);
 
     //Update the sizes
-    header->size() = blocks * BLOCK_SIZE - META_SIZE;
-    header->footer()->size() = header->size();
+    auto size = blocks * BLOCK_SIZE - META_SIZE;
+    header->size() = size;
+    header->footer()->size() = size;
 
     //When created a block has no left and right neighbour
     header->set_left(false);
@@ -242,6 +295,8 @@ void expand_heap(malloc_header_chunk* current, size_t bytes = 0){
 
     //Insert the new block into the free list
     insert_after(current, header);
+
+    check_node(header, "header after expand_head");
 }
 
 malloc_header_chunk* left_block(malloc_header_chunk* b){
@@ -291,6 +346,8 @@ malloc_header_chunk* coalesce(malloc_header_chunk* b){
 
         b->set_left(block_left);
         b->set_right(block_right);
+
+        check_node(b, "b after coalesce");
     }
 
     if(c && c->is_free()){
@@ -307,6 +364,8 @@ malloc_header_chunk* coalesce(malloc_header_chunk* b){
 
         b->set_left(block_left);
         b->set_right(block_right);
+
+        check_node(b, "b after coalesce");
     }
 
     return b;
@@ -351,7 +410,7 @@ void kalloc::finalize(){
 }
 
 void* kalloc::k_malloc(uint64_t bytes){
-    direct_int_lock lock;
+    direct_int_lock l;
 
     auto current = malloc_head->next();
 
@@ -393,11 +452,11 @@ void* kalloc::k_malloc(uint64_t bytes){
 
                 //Create a new block inside the current one
                 auto new_block = reinterpret_cast<malloc_header_chunk*>(
-                    reinterpret_cast<uintptr_t>(current) + bytes + META_SIZE);
+                    reinterpret_cast<uintptr_t>(static_cast<const void*>(current)) + bytes + META_SIZE);
 
                 //Update the size of the new block
                 new_block->size() = new_block_size;
-                new_block->footer()->size() = new_block->size();
+                new_block->footer()->size() = new_block_size;
 
                 new_block->set_left(true);
                 new_block->set_right(block_right);
@@ -408,14 +467,19 @@ void* kalloc::k_malloc(uint64_t bytes){
                 //Remove the current block from the free list
                 remove(current);
 
-                debug_malloc<DEBUG_MALLOC>("after malloc split");
+                check_node(current, "current after malloc split");
+                check_node(new_block, "new_block after malloc split");
+
+                debug_malloc("after malloc split");
 
                 break;
             } else {
                 //Remove this node from the free list
                 remove(current);
 
-                debug_malloc<DEBUG_MALLOC>("after malloc no split");
+                check_node(current, "current after malloc no split");
+
+                debug_malloc("after malloc no split");
 
                 break;
             }
@@ -443,10 +507,7 @@ void kalloc::k_free(void* block){
     auto free_header = reinterpret_cast<malloc_header_chunk*>(
         reinterpret_cast<uintptr_t>(block) - sizeof(malloc_header_chunk));
 
-    if(free_header->is_free()){
-        logging::logf(logging::log_level::ERROR, "kalloc: free block getting freed\n");
-        k_print_line("ERROR: free block getting freed");
-    }
+    thor_assert(!free_header->is_free(), "kalloc: free block getting freed\n");
 
     if(TRACE_MALLOC){
         logging::logf(logging::log_level::TRACE, "f %u %h\n", free_header->size(), reinterpret_cast<uint64_t>(block));
@@ -461,7 +522,9 @@ void kalloc::k_free(void* block){
     //Add the freed block in the free list
     insert_after(malloc_head, free_header);
 
-    debug_malloc<DEBUG_MALLOC>("after free");
+    check_node(free_header, "block after free");
+
+    debug_malloc("after free");
 }
 
 size_t kalloc::allocated_memory(){
@@ -498,6 +561,7 @@ void kalloc::debug(){
 
     printf("malloc overhead: %u\n", META_SIZE);
     k_print("Free blocks:");
+
     do {
         if(!it->is_free()){
             ++non_free_blocks;
@@ -515,6 +579,7 @@ void kalloc::debug(){
     } while(it != malloc_head);
 
     k_print_line();
+
     printf("memory free in malloc chain: %m (%u)\n", memory_free, memory_free);
     printf("There are %u non free blocks in the free list\n", non_free_blocks);
     printf("There are %u inconsistent sized blocks in the free list\n", inconsistent);
