922406f6ba59f0ec446f162e617076965dbe82ec
Baptiste Wicht <baptiste.wicht@gmail.com>
2018-04-04 09:32:15 +0200

    Update copyright

--- kernel/include/bitmap.hpp
+++ kernel/include/bitmap.hpp
@@ -1,5 +1,6 @@
 //=======================================================================
-// Copyright Baptiste Wicht 2013-2016.  // Distributed under the terms of the MIT License.
+// Copyright Baptiste Wicht 2013-2018.
+// Distributed under the terms of the MIT License.
 // (See accompanying file LICENSE or copy at
 //  http://www.opensource.org/licenses/MIT)
 //=======================================================================
--- tstl/include/memory.hpp
+++ tstl/include/memory.hpp
@@ -1,5 +1,5 @@
 //=======================================================================
-// Copyright Baptiste Wicht 2013-2016.
+// Copyright Baptiste Wicht 2013-2018.
 // Distributed under the terms of the MIT License.
 // (See accompanying file LICENSE or copy at
 //  http://www.opensource.org/licenses/MIT)
