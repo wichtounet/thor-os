751222c228081e9553c769295f9b4762d3560357
Baptiste Wicht <baptiste.wicht@gmail.com>
2018-04-06 08:19:21 +0200

    Allow to disable assertions

--- kernel/src/assert.cpp
+++ kernel/src/assert.cpp
@@ -6,24 +6,36 @@
 //=======================================================================
 
 #include "assert.hpp"
+
+#ifndef THOR_NO_ASSERT
 #include "print.hpp"
 #include "kernel.hpp"
 #include "logging.hpp"
+#endif
 
 void __thor_assert(bool condition){
     __thor_assert(condition, "assertion failed");
 }
 
 void __thor_assert(bool condition, const char* message){
+#ifndef THOR_NO_ASSERT
     if(!condition){
         logging::logf(logging::log_level::ERROR, "Assertion failed: %s\n", message);
         k_print_line(message);
         suspend_kernel();
     }
+#else
+    (void) condition;
+    (void) message;
+#endif
 }
 
 void __thor_unreachable(const char* message){
+#ifndef THOR_NO_ASSERT
     logging::logf(logging::log_level::ERROR, "Reached unreachable block: %s\n", message);
     k_print_line(message);
     suspend_kernel();
+#else
+    (void) message;
+#endif
 }
